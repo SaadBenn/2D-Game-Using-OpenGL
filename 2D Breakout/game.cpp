4add9bfe38c37f0a33fe596039e707a3d090a4a4
Saad <saadbennmushtaq@gmail.com>
2018-11-17 17:51:49 -0500

    Setup Game class structure

---
+++ 2D Breakout/game.cpp
@@ -0,0 +1,31 @@
+//
+//  game.cpp
+//  2D Breakout
+//
+//  Created by Saad Mushtaq on 2018-11-17.
+//  Copyright © 2018 Saad Mushtaq. All rights reserved.
+//
+
+#include <stdio.h>
+#include "game.h"
+#include "resource_manager.h"
+
+Game::Game(GLuint width, GLuint height) : State(Game_Active), Keys(), Width(width), Height(height) {
+    
+}
+
+Game::~Game() {
+    
+}
+
+void Game::Init() {
+    
+}
+
+void Game::Update(<#GLfloat dt#>) {
+    
+}
+
+void Game::Render() {
+    
+}
--- 2D Breakout/game.h
+++ 2D Breakout/game.h
@@ -8,6 +8,7 @@
 
 #ifndef game_h
 #define game_h
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
