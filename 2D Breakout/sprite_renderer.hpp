be8aeacb2d23fd24554d46ee3bf5b35e9cb7c9ea
Saad <saadbennmushtaq@gmail.com>
2018-11-17 22:50:15 -0500

    Setup sprite renderer structure

---
+++ 2D Breakout/sprite_renderer.hpp
@@ -0,0 +1,14 @@
+//
+//  sprite_renderer.hpp
+//  2D Breakout
+//
+//  Created by Saad Mushtaq on 2018-11-17.
+//  Copyright © 2018 Saad Mushtaq. All rights reserved.
+//
+
+#ifndef sprite_renderer_hpp
+#define sprite_renderer_hpp
+
+#include <stdio.h>
+
+#endif /* sprite_renderer_hpp */
